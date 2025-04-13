a21beb87b6303c111b1d364b8ad725a38452f44c
NamMai195 <mnam3239@gmail.com>
2025-04-13 11:49:50 +0700

    Add initial AuthenticationServiceTest class

---
+++ src/test/java/com/backend/service/AuthenticationServiceTest.java
@@ -0,0 +1,6 @@
+package com.backend.service;
+
+public class AuthenticationServiceTest {
+}
+
+//Generated with love by TestMe :) Please raise issues & feature requests at: https://weirddev.com/forum#!/testme
